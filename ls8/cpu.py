764c1800fd9922d86dcf1d42b534d9f328e6721f
Jnmendza <jonatmendoza@gmail.com>
2020-04-20 14:27:41 -0700

    0-2 steps completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,18 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # construct RAM REG and PC
+        # This builds 8 slots in the list [R0, R1, R2, etc]
+        self.reg = [0] * 8
+        # This builds 256 slots in the list for memory
+        self.ram = [0] * 256
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +24,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +53,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -63,3 +68,16 @@ class CPU:
     def run(self):
         """Run the CPU."""
         pass
+
+    def ram_read(self, mar):
+        """
+        mar - Memory Address Register read or written
+        mdr - Memory Data Register that was read or written
+        """
+        # Where in the 256 individual slots of RAM are you looking?
+        # When you find it, what is the value? Return the Value
+        mdr = self.ram[mar]
+        return mdr
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
